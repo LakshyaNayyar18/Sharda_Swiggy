a68556e17fdc967455aeff16bf36bfb9a585845f
LakshyaNayyar18 <LakshyaNayyar18@gmail.com>
2023-06-14 13:07:15 +0530

    commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,61 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String[] menuItems = {
+                "1. Chiken Tikka - 1099",
+                "2. Panner masala - 299",
+                "3. Rajma Chawla - 899",
+                "4. Rasgulla - 699",
+                "5. Rasmalai - 799",
+                "6. Exit"
+        };
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // Display the menu
+        System.out.println("Welcome to the Restaurant!");
+        System.out.println("Menu:");
+
+        for (String item : menuItems) {
+            System.out.println(item);
         }
+
+        // Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to make a selection
+        System.out.print("Enter the number corresponding to your choice: ");
+        int choice = scanner.nextInt();
+
+        // Process the user's choice
+        switch (choice) {
+            case 1:
+                System.out.println("You selected Chicken Tikka. Price: 1099. Enjoy your meal!");
+                break;
+            case 2:
+                System.out.println("You selected Panner Masala. Price: 299. Enjoy your meal!");
+                break;
+            case 3:
+                System.out.println("You selected Rajma Chawla. Price: 899. Enjoy your meal!");
+                break;
+            case 4:
+                System.out.println("You selected Rasgulla. Price: 699. Enjoy your meal!");
+                break;
+            case 5:
+                System.out.println("You selected Rasmali. Price: 799. Enjoy your meal!");
+                break;
+            case 6:
+                System.out.println("Thank you for visiting the restaurant. Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid option.");
+                break;
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
